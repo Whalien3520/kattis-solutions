05401400bd6400460bc89370742f35616439b2ca
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2024-06-01 11:18:20 -0500

    Create kvedja.java

---
+++ java/kvedja.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class kvedja {
+	public static void main(String[] args) throws IOException {
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		String s = r.readLine();
+		System.out.println("Kvedja,");
+		System.out.println(s);
+	}
+}
