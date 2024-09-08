ffbf91c70a52b11b002d11ef41a1dbfa3afe6180
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2024-09-07 22:55:43 -0500

    Create leynithjonusta.java

---
+++ java/leynithjonusta.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class leynithjonusta
+{
+	public static void main(String[] args) throws IOException {
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = r.readLine().split(" ");
+		for(String s : arr)
+		    System.out.print(s);
+	}
+}
