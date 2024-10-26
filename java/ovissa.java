bc77a5eacf85b85163caf1712406973f1e62cd3a
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2024-10-25 22:54:26 -0500

    Create ovissa.java

---
+++ java/ovissa.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+public class ovissa {
+    public static void main(String[] args) throws IOException {
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(scan.readLine().length());
+    }
+}
