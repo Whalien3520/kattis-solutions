d1ab338ec4958000206fd5232559fda185a3869e
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2024-11-06 21:33:07 -0600

    Create tolvunarfraedingartelja.java

---
+++ java/tolvunarfraedingartelja.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class tolvunarfraedingartelja {
+	public static void main(String[] args) throws IOException {
+		Scanner s = new Scanner(System.in);
+		int i = s.nextInt();
+		System.out.println(i - 1);
+		s.close();
+	}
+}
