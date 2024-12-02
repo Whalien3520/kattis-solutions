9f8a81c62e39cf8c97dc24672159ef31a7c4b99f
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2024-12-02 10:56:36 -0600

    Create dragafra.java

---
+++ java/dragafra.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class dragafra {
+	public static void main(String[] args) throws IOException {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt(), m = s.nextInt();
+		System.out.println(n - m);
+		s.close();
+	}
+}
