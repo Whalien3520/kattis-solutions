3b5d9dd026eebecd266019ba88c032f831fa920a
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-06-02 17:09:00 -0500

    Create skeytasaman.java

---
+++ java/skeytasaman.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class skeytasaman {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readine(), t = br.readLine();
+		System.out.println(s + t);
+	}
+}
