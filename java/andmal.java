5b44a0e557751e55ad509605798054e401a9e927
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-07-01 08:11:29 -0500

    Create andmal.java

---
+++ java/andmal.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class andmal {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println((s.equals("")) ? "a" : "");
+    }
+}
