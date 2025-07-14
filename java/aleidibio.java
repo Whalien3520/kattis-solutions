98368794c8f305cc8c690015489782ed937945aa
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-07-13 23:58:34 -0500

    Update aleidibio.java
    Minor formatting changes

--- java/aleidibio.java
+++ java/aleidibio.java
@@ -1,9 +1,11 @@
 import java.io.*;
+import java.util.*;
+
 public class aleidibio {
 	public static void main(String[] args) throws IOException {
-		BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
-		int a = Integer.parseInt(scan.readLine()), b = Integer.parseInt(scan.readLine()), c = Integer.parseInt(scan.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine()), b = Integer.parseInt(scan.readLine()), c = Integer.parseInt(scan.readLine());
 		System.out.println(c - b - a);
-		scan.close();
+		br.close();
 	}
 }
