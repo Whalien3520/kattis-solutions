c0734a48bdddec72353852152638b94c572fae56
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-07-13 23:55:53 -0500

    Create zamka.java

---
+++ java/zamka.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class zamka {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine()), D = Integer.parseInt(br.readLine()), X = Integer.parseInt(br.readLine()), N = L, M = D;
+        while(digitSum(N) != X)
+            N++;
+        while(digitSum(M) != X)
+            M--;
+        System.out.println(N);
+        System.out.println(M);
+    }
+    private static int digitSum(int a) {
+        int ret = 0;
+        while(a > 0) {
+            ret += a % 10;
+            a /= 10;
+        }
+        return ret;
+    }
+}
