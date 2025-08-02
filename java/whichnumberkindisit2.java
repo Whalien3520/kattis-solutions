d84c7e0d54761b512f77d95076326cb4690558c9
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-08-01 23:51:54 -0500

    Create whichnumberkindisit2.java

---
+++ java/whichnumberkindisit2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class whichnumberkindisit2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0) {
+            int N = Integer.parseInt(br.readLine()), r = (int)Math.sqrt(N);
+            if(N % 2 == 1)
+                System.out.print("O");
+            if(r * r == N)
+                System.out.print("S");
+            else if(N % 2 == 0)
+                System.out.print("EMPTY");
+            System.out.println();
+        }
+    }
+}
