5e116aead6ef2f060949ca28405dac1634c40f7d
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-08-19 13:05:57 -0500

    Create flatterland.java

---
+++ java/flatterland.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class flatterland {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()), x = Integer.parseInt(br.readLine());
+        System.out.println((n - 1) * x);
+    }
+}
