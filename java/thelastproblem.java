63afe9904547b6f45a4641b266cce58460dad82d
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-09-03 23:45:26 -0500

    Create thelastproblem.java

---
+++ java/thelastproblem.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class thelastproblem {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println("Thank you, " + s + ", and farewell!");
+    }
+}
