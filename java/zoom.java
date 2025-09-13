d1449570efaf3d0898191b6f38a5644d48b46a9d
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-09-13 07:30:32 -0500

    Create zoom.java

---
+++ java/zoom.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class zoom {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] nk = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray(), x = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for(int i = nk[1] - 1; i < nk[0]; i += nk[1])
+            System.out.print(x[i] + " ");
+    }
+}
