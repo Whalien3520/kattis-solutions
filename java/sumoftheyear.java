4b7b34e8864951a4c047c9d84e70305fe76aa454
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-10-12 23:11:52 -0500

    Create sumoftheyear.java

---
+++ java/sumoftheyear.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class sumoftheyear {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+    }
+}
