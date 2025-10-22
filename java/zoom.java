e8b3ce470416d044f15419aad2e182491a4ee7df
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-10-21 23:38:32 -0500

    Edit zoom.java
    Minor formatting change

--- java/zoom.java
+++ java/zoom.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class zoom {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] nk = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray(), x = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         for(int i = nk[1] - 1; i < nk[0]; i += nk[1])
             System.out.print(x[i] + " ");
