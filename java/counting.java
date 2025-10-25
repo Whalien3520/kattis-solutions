5eae3cf5fa8d9849682f090d5b654343775490dd
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-10-24 23:58:00 -0500

    Edit counting.java
    Minor formatting changes

--- java/counting.java
+++ java/counting.java
@@ -1,9 +1,10 @@
 import java.io.*;
+import java.math.*;
 import java.util.*;
 
 public class counting {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         for(int i = 1; i <= 12; i++)
             System.out.println(i * n);
