90c928dcb0329d03d37b7c01867a6ebe1b0cffa6
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-11-06 21:55:18 -0600

    Edit abovesealevel.java
    Minor formatting changes

--- java/abovesealevel.java
+++ java/abovesealevel.java
@@ -1,9 +1,12 @@
 import java.io.*;
+import java.math.*;
 import java.util.*;
 
 public class abovesealevel {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter br = new BufferedWriter(new InputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(Double.parseDouble(br.readLine()) - 0.3);
+        bw.write(String.format("%f\n", Double.parseDouble(br.readLine()) - 0.3));
+        bw.flush();
     }
 }
