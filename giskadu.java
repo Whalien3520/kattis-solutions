430125ec1d78e5e901a081d3251d5346e7a3e288
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2025-12-02 21:42:33 -0600

    Create giskadu.java

---
+++ giskadu.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class giskadu {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        System.out.println(273);
+    }
+}
